508b50caa87568b7391a004885106962c8227839
Djinhljnh <125116549+Djinhljnh@users.noreply.github.com>
2024-09-24 15:15:47 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World! Welcome to my simple Flask web page."
-
-if __name__ == '__main__':
-    app.run(debug=True)
+for i in range(1, 11):
+print(i)
